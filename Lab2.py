7720d3eba1295e796fc3939189ab9a5b34c1cdad
FranciscoJavierRAM <pacorro-2014@hotmail.com>
2025-05-20 19:45:02 -0400

    Add files via upload

---
+++ Lab2.py
@@ -0,0 +1,10 @@
+# Step 1 ask for the users first pet's name
+print("What is your first pets name?")
+#Step 2 input the pets name and save it for later use
+pet_name = input()
+#Step 3 Ask how old were you when you got (pets name)
+print ("How old were you when you got " + str(pet_name))
+#Step 4 Input the users age when they got their first pet
+age = input()
+#Step 5 Say "Wow you were only (users age) when you got (pets name). Thats awesome."
+print ("Wow, you were only " + str(age) + " when you got " + str(pet_name) + "." + " That's awesome.")
